aee2a742666bc7abaa4da70549f343b2df0d8f31
fgom-dev <fnd.gomes02@gmail.com>
2021-06-30 08:05:48 -0300

    EstudoXML

---
+++ EstudoXML/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace EstudoXML
+{
+    partial class frmAgenda
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lbxContatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(531, 37);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "label1";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbxContatos
+            // 
+            this.lbxContatos.FormattingEnabled = true;
+            this.lbxContatos.ItemHeight = 15;
+            this.lbxContatos.Location = new System.Drawing.Point(12, 49);
+            this.lbxContatos.Name = "lbxContatos";
+            this.lbxContatos.Size = new System.Drawing.Size(508, 214);
+            this.lbxContatos.TabIndex = 1;
+            // 
+            // frmAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 270);
+            this.Controls.Add(this.lbxContatos);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmAgenda";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.frmAgenda_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox lbxContatos;
+    }
+}
+
---
+++ EstudoXML/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace EstudoXML
+{
+    public partial class frmAgenda : Form
+    {
+        public frmAgenda()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAgenda_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = CarregarTitulo();
+            CarregarContatos();
+        }
+
+        private string CarregarTitulo()
+        {
+            XmlDocument documentoXML = new XmlDocument();
+            documentoXML.Load(@"C:\Users\usuario022\source\repos\TreinaWeb.CSharp.Intermediario\EstudoXML\Agenda.xml");
+            XmlNode noTitulo = documentoXML.SelectSingleNode("/agenda/titulo");
+            return noTitulo.InnerText;            
+        }
+
+        private void CarregarContatos()
+        {
+            XmlDocument documentoXML = new XmlDocument();
+            documentoXML.Load(@"C:\Users\usuario022\source\repos\TreinaWeb.CSharp.Intermediario\EstudoXML\Agenda.xml");
+            XmlNodeList contatos = documentoXML.SelectNodes("/agenda/contatos/contato");
+            foreach (XmlNode contato in contatos)
+            {
+                string representacaoContato = "";
+                string id = contato.Attributes["id"].Value;
+                string nome = contato.Attributes["nome"].Value;
+                string idade = contato.Attributes["idade"].Value;
+                representacaoContato = nome + ", " + idade + ", " + id;
+                lbxContatos.Items.Add(representacaoContato);
+            }
+        }
+    }
+}
